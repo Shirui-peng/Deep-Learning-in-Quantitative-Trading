73d3157538b484fcb9529dd7d44e8bdf773db550
Zihao Zhang <zhangzihao@hotmail.co.uk>
2025-03-13 22:12:48 +0800

    commit chapter 5, 6, 7

--- Utilis/early_stopper.py
+++ Utilis/early_stopper.py
@@ -1,9 +1,11 @@
+import torch 
+
 class EarlyStopping:
     """
     Early stops the training if validation loss doesn't improve 
     after a given patience.
     """
-    def __init__(self, patience=5, min_delta=0.0, verbose=False):
+    def __init__(self, savepath, patience=5, min_delta=0.0, verbose=False):
         """
         Args:
             patience  (int): How many epochs to wait after last improvement.
@@ -13,12 +15,13 @@ class EarlyStopping:
         self.patience = patience
         self.min_delta = min_delta
         self.verbose = verbose
+        self.savepath = savepath
         
         self.counter = 0
         self.best_loss = None
         self.early_stop = False
 
-    def __call__(self, val_loss):
+    def __call__(self, model, val_loss):
         # If no best_loss set yet, treat current as best
         if self.best_loss is None:
             self.best_loss = val_loss
@@ -26,6 +29,7 @@ class EarlyStopping:
         elif (self.best_loss - val_loss) > self.min_delta:
             self.best_loss = val_loss
             self.counter = 0
+            torch.save(model.state_dict(), self.savepath)
             if self.verbose:
                 print(f"Validation loss improved. Resetting counter.")
         else:
